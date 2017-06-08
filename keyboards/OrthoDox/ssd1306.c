bf5971364334b3ea09bc73c189b7a04a2692d133
Mathieu Dupuis <mat.dupuis3@gmail.com>
2017-06-07 22:10:11 -0700

    Update ssd1306.c

--- keyboards/OrthoDox/ssd1306.c
+++ keyboards/OrthoDox/ssd1306.c
@@ -5,7 +5,7 @@
 #include "i2c.h"
 #include <string.h>
 #include "print.h"
-#include "lets_split.h"
+#include "orthodox.h"
 #include "common/glcdfont.c"
 #ifdef ADAFRUIT_BLE_ENABLE
 #include "adafruit_ble.h"
